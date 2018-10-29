90e6fa7cd524e1de4f4bf1bbb94a787dd6342289
Thai Nguyen <thaiomega3@gmail.com>
2018-10-29 15:13:58 -0500

    Initial Commit

---
+++ Image Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Gallery
+//
+//  Created by Thai Nguyen on 10/29/18.
+//  Copyright © 2018 Thai Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
